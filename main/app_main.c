dae01390cddc4b36042bafe7922e253ca1772087
koson <567256+koson@users.noreply.github.com>
2023-09-18 04:04:59 +0700

    Update app_main.c

--- main/app_main.c
+++ main/app_main.c
@@ -368,7 +368,7 @@ void app_main(void)
     setup_websocket_server();
 
     while (1) {
-        ESP_LOGI(TAG, "Hello World!");
+        //ESP_LOGI(TAG, "Hello World!");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
